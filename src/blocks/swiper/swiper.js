21e8a1a87f81112678244025717144db2d9c0268
Elena Vsiakikh <elenvsiakikh@gmail.com>
2020-08-04 21:14:10 +0400

    Page About is finished

--- src/blocks/swiper/swiper.js
+++ src/blocks/swiper/swiper.js
@@ -5,6 +5,23 @@ var swiper = new Swiper('.swiper-container', {
     centeredSlides: true,
     spaceBetween: 16,
     slidesPerGroup: 3,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        centeredSlides: false,
+      },
+      500: {
+        slidesPerView: 2.2,
+        slidesPerGroup: 2,
+        spaceBetween: 8,
+        centeredSlides: false,
+      },
+      1024: {
+        slidesPerView: 3.5,
+        slidesPerGroup: 3,
+        spaceBetween: 16
+      }
+    },
     loop: true,
     loopFillGroupWithBlank: false,
     pagination: {
